d04d123f97b345f87886505ff6558b15d25ef6d3
ryotti1225 <ryosei.o.91803@gmail.com>
2025-04-18 18:15:54 +0900

    isa

--- 一年生歓迎クライアント側/ソース.cpp
+++ 一年生歓迎クライアント側/ソース.cpp
@@ -1,37 +1,48 @@
 ﻿#include <boost/asio.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
 
 using boost::asio::ip::tcp;
 
+void read_messages(tcp::socket& socket) {
+    try {
+        for (;;) {
+            char data[1024];
+            std::memset(data, 0, sizeof(data));
+
+            // サーバーからのメッセージを受信
+            size_t length = socket.read_some(boost::asio::buffer(data));
+            std::cout << "Server: " << std::string(data, length) << std::endl;
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << "Connection closed: " << e.what() << std::endl;
+    }
+}
+
 int main() {
     try {
-        // io_contextの作成
         boost::asio::io_context io_context;
 
-        // リゾルバでホスト名とポートを解決
-        tcp::resolver resolver(io_context);
-        auto endpoints = resolver.resolve("127.0.0.1", "12345");
-
-        // ソケットの作成
+        // サーバーに接続
         tcp::socket socket(io_context);
-
-        // サーバに接続
-        boost::asio::connect(socket, endpoints);
-
+        tcp::resolver resolver(io_context);
+        boost::asio::connect(socket, resolver.resolve("127.0.0.1", "12345"));
         std::cout << "Connected to the server!" << std::endl;
 
-        // サーバに送信するメッセージ
-        std::string message = "Hello from client!";
-        boost::asio::write(socket, boost::asio::buffer(message));
+        // 別スレッドでサーバーからのメッセージを受信
+        std::thread reader_thread(read_messages, std::ref(socket));
 
-        // サーバからの応答を受信
-        char reply[1024];
-        size_t reply_length = socket.read_some(boost::asio::buffer(reply));
-        std::cout << "Reply from server: ";
-        std::cout.write(reply, reply_length);
-        std::cout << std::endl;
+        // メッセージを送信
+        for (;;) {
+            std::string message;
+            std::cout << "You: ";
+            std::getline(std::cin, message);
+            boost::asio::write(socket, boost::asio::buffer(message));
+        }
 
+        reader_thread.join();
     }
     catch (std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
